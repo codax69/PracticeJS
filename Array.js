f46190f198982bee2fb35aeee7f9d93767238e62
codax69 <128021116+codax69@users.noreply.github.com>
2024-03-12 13:48:56 +0530

    Array

--- Array.js
+++ Array.js
@@ -66,3 +66,69 @@ const lastElement = (arr, n) => arr.slice(arr.length - 1, n);
 
 // ! console.log("mainArray:" + lastElement(originArr));
 // ! console.log(lastElement([], 3));
+
+// // 5. **Join Array Elements into a String:**
+// - Write a program that joins all elements of an array into a string.
+// - Example:**JavaScript**
+    
+//     ```jsx
+    const myColor = ['Red', 'Green', 'White', 'Black'];
+//     console.log(myColor.join(',')); // "Red,Green,White,Black"**
+//     ```
+    
+//     ```jsx
+//     console.log(myColor.join(',')); // "Red,Green,White,Black"**
+//     ```
+
+const joinElement = (arr)=>arr.map(arr=>arr.toLowerCase()).join(',')
+console.log(joinElement(myColor))
+
+
+// 6. **Insert Dashes Between Even Numbers:**
+//     - Accept a number as input and insert dashes (-) between each even number.
+//     - Example:
+        
+//         ```jsx
+//         **Input: 025468
+//         Output: 0-254-6-8**
+//         ```
+// const dashBetweenEven = (num) => {
+//   let arr = num.toString().split('');
+//   let result = "";
+//   for(let i = 0; i < arr.length ; i++){
+//     const digit = parseInt(arr[i]);
+//     if(digit % 2 === 0 && parseInt(arr[i+1]) % 2 === 0){
+//       result += digit + '-';
+//     } else{
+//       result += digit;
+//     }
+//   }
+//   return result;
+// }
+// console.log(dashBetweenEven(4567653));
+
+// 7. **Sort an Array:**
+// - Create a program that sorts the items of an array.
+// - Example:**JavaScript**
+    
+//     ```jsx
+//     **
+//     console.log(arr1.sort((a, b) => a - b)); // -4, -3, 1, 2, 3, 5, 6, 7, 8**
+//     ```
+const arr = [-3, 8, 7, 6, 5, -4, 3, 2, 1];
+const sortArray =(arr)=>{
+  return arr.sort()
+}
+console.log(sortArray(arr))
+
+// 8. **Find the Most Frequent Item in an Array:**
+//     - Write a program that identifies the most frequent item in an array.
+//     - Example:**JavaScript**
+        
+//         ```jsx
+//         **const arr1 = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+//         console.log(findMostFrequent(arr1)); // 'a' (5 times)**
+//         ```
+        
+        const swapCase = (input)=> input.split('').map((char)=>(char === char.toLowerCase()? char.toUpperCase():char.toLowerCase())).join('')
+        console.log(swapCase('The Quick Brown Fox'))
